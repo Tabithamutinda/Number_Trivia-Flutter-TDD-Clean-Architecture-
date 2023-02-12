2d2e780b55a9af6e8feb9f625b0e85bff1db7e8d
Tabithamutinda <tabbithamutinda@gmail.com>
2023-02-12 20:58:45 +0300

    Implementing Params in here

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,17 +1,26 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 import 'package:numbers_trivia/core/error/failures.dart';
+import 'package:numbers_trivia/core/usecases/usecases.dart';
 import 'package:numbers_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:numbers_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
   final NumberTriviaRepository repository;
 
   GetConcreteNumberTrivia(this.repository);
 
-  Future<Either<Failure, NumberTrivia>> call({
-    @required int number,
-  }) async {
-    return await repository.getConcretenumberTrivia(number);
+  Future<Either<Failure, NumberTrivia>> call(Params params) async {
+    return await repository.getConcretenumberTrivia(params.number);
   }
+}
+
+class Params extends Equatable {
+  final int number;
+
+  Params({@required this.number});
+
+  @override
+  List<Object> get props => [number];
 }
