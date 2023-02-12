9239e405a7b771f061c1218f3c082c625dc525a9
Tabithamutinda <tabbithamutinda@gmail.com>
2023-02-12 20:59:10 +0300

    Creation of get random number trivia usecase

---
+++ test/get_random_number_trivia_test.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:numbers_trivia/core/usecases/usecases.dart';
+import 'package:numbers_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:numbers_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:numbers_trivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
+
+void main() {
+  GetRandomNumberTrivia usecase;
+  MockNumberTriviaRepository mockNumberTriviaRepository;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetRandomNumberTrivia(mockNumberTriviaRepository);
+  });
+
+  final tNumber = 1;
+  final tNumberTrivia = NumberTrivia(number: 1, text: 'test');
+
+  test('should get trivia for the number from the repository', () async {
+    when(mockNumberTriviaRepository.getRandomNumberTrivia())
+        .thenAnswer((_) async => Right(tNumberTrivia));
+    final result = await usecase(NoParams());
+    //usecase should simply return whatever was returned from the repository.
+    expect(result, Right(tNumberTrivia));
+    //verify that the method should be called and nothing more.
+    verify(mockNumberTriviaRepository.getRandomNumberTrivia());
+    //only the above methos should be called and nothing more.
+    verifyNoMoreInteractions(mockNumberTriviaRepository);
+  });
+}
